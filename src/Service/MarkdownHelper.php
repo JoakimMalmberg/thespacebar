6655769a697624a275fd96dd6064f0c45b013388
Joakim <jmalmberg.web@gmail.com>
2019-07-29 21:28:35 +0200

    Services, Config & Enviroments 9

---
+++ src/Service/MarkdownHelper.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace App\Service;
+
+
+use Michelf\MarkdownInterface;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+
+class MarkdownHelper
+{
+    /**
+     * @var AdapterInterface
+     */
+    private $cache;
+
+    /**
+     * @var MarkdownInterface
+     */
+    private $markdown;
+
+    /**
+     * MarkdownHelper constructor.
+     * @param AdapterInterface $cache
+     * @param MarkdownInterface $markdown
+     */
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown)
+    {
+        $this->markdown = $markdown;
+        $this->cache = $cache;
+    }
+
+    public function parse(string $source): string
+    {
+        $item = $this->cache->getItem('markdown_'.md5($source));
+        if (!$item->isHit()) {
+            $item->set($this->markdown->transform($source));
+            $this->cache->save($item);
+        }
+        return $item->get();
+    }
+}
